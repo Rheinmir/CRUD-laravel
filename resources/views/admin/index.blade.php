993887b3515d68f0845bc22e8f23f5d5d99a7f80
Rheinmir <rheinmir@gmail.com>
2024-04-12 20:51:44 +0700

    figma test

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
 
-<div class="jumbotron">
+<!-- <div class="jumbotron">
             <div class="container">
                 <h1>Hello</h1>
                 <p>Contents ...</p>
@@ -10,6 +10,136 @@
                     <a class="btn btn-primary btn-lg">Learn more</a>
                 </p>
             </div>
+        </div> -->
+
+
+        <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Landing Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/landingPage.css'); }} " />
+  </head>
+  <body>
+    <div class="landing-page">
+      <div class="container-13">
+        <div class="container-1">
+          <div class="container-2">
+            <span class="pr">
+            Pr
+            </span>
+          </div>
+          <div class="pro-skills">
+          Pro-Skills
+          </div>
         </div>
+        <div class="group-2">
+          <span class="home">
+          Home
+          </span>
+          <span class="classes">
+          Classes
+          </span>
+          <span class="plans">
+          Plans
+          </span>
+          <span class="about-us">
+          About Us
+          </span>
+        </div>
+        <div class="container-16">
+          <div class="container-10">
+            <span class="login">
+            Login
+            </span>
+          </div>
+          <div class="container">
+            <span class="sign-in">
+            Sign In
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="container-6">
+        <div class="container-7">
+          <div class="learn-anew-skill-everyday-anytime-and-anywhere">
+          Learn a New Skill<br />
+          Everyday, Anytime, and Anywhere.
+          </div>
+          <p class="courses-covering-all-tech-domains-for-you-to-learn-and-explore-new-oppurtunities-learn-from-industry-experts-and-land-your-dream-job">
+          <span class="courses-covering-all-tech-domains-for-you-to-learn-and-explore-new-oppurtunities-learn-from-industry-experts-and-land-your-dream-job-sub-26"></span><span></span>
+          </p>
+          <div class="container-11">
+            <div class="container-14">
+              <span class="start-trial">
+              Start Trial
+              </span>
+            </div>
+            <div class="how-it-works">
+            How it Works
+            </div>
+          </div>
+          <div class="container-12">
+            <span class="container">
+            1000+
+            </span>
+            <span class="container-2">
+            5000+
+            </span>
+            <span class="container-1">
+            200+
+            </span>
+          </div>
+          <div class="container-9">
+            <span class="courses-to-choose-from">
+            Courses to <br />
+            choose from
+            </span>
+            <span class="students-trained">
+            Students <br />
+            Trained
+            </span>
+            <span class="professional-trainers">
+            Professional<br />
+            Trainers
+            </span>
+          </div>
+          <img class="vector-6" src="../assets/vectors/vector6_x2.svg" />
+          <div class="rectangle-2">
+          </div>
+        </div>
+        <div class="group-1">
+          <div class="container-4">
+            <div class="ellipse-8">
+            </div>
+            <div class="mask-group">
+              <div class="happy-young-woman-holding-laptop-gray-wall-transformed-1">
+              </div>
+            </div>
+          </div>
+          <div class="happy-young-woman-holding-laptop-gray-wall-transformed-2">
+          </div>
+        </div>
+        <div class="removebg-preview-11">
+        </div>
+        <img class="container-3" src="../assets/vectors/container2_x2.svg" />
+        <img class="container-15" src="../assets/vectors/container_x2.svg" />
+      </div>
+      <div class="removebg-preview-1">
+      </div>
+    </div>
+  </body>
+</html>
+
+
 
 @stop()
+
